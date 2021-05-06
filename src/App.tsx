6b5510f748c12ba81a745df87cabba9feb8f4ef7
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-05-06 20:06:34 -0300

    feat: add NewTransactionModal component

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,34 @@
+import { useState } from 'react';
+
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import NewTransactionModal from './components/NewTransactionModal';
 
 import { GlobalStyle } from "./styles/global";
 
+
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionsModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionsModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionsModalOpen(false);
+  }
+
   return (
     <>
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+
       <Dashboard />
+
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
       <GlobalStyle />
     </>
   );
