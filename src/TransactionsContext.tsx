8acaeb10d89287b76f732e9449c72a4efe22a8fe
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-05-07 20:51:33 -0300

    feat: add createTransaction in TransactionsProvider

--- src/TransactionsContext.tsx
+++ src/TransactionsContext.tsx
@@ -5,17 +5,24 @@ import { api } from "./services/api";
 interface Transaction {
   id: number;
   title: string;
-  type: 'deposit' | 'withdraw';
+  type: string;
   amount: number;
   category: string;
   createdAt: string;
 }
 
+type TransactionInput = Omit<Transaction, 'id' | 'createdAt'>
+
 interface TransactionsProviderProps {
-  children: ReactNode
+  children: ReactNode;
 }
 
-export const TransactionsContext = createContext<Transaction[]>([])
+interface TransactionsContextData {
+  transactions: Transaction[];
+  createTransaction: (transaction: TransactionInput) => void;
+}
+
+export const TransactionsContext = createContext<TransactionsContextData>({} as TransactionsContextData)
 
 export function TransactionsProvider({ children }: TransactionsProviderProps) {
   const [transactions, setTransactions] = useState<Transaction[]>([])
@@ -24,8 +31,12 @@ export function TransactionsProvider({ children }: TransactionsProviderProps) {
     api.get('/transactions').then(response => setTransactions(response.data.transactions))
   }, [])
 
+  function createTransaction(transaction: TransactionInput) {
+    api.post('/transactions', transaction)
+  }
+
   return (
-    <TransactionsContext.Provider value={transactions}>
+    <TransactionsContext.Provider value={{ transactions, createTransaction }}>
       {children}
     </TransactionsContext.Provider>
   )
